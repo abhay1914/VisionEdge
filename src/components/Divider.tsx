fc42df9261fc09001c92a9655fc3576b5c485e79
SoumyaK <soumyakandagal7@gmail.com>
2026-09-19 19:30:24 +0530

    add reusable UI divider

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+export default function Divider() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "1px",
+        background: "#1e293b",
+        margin: "20px 0",
+      }}
+    />
+  );
+}
